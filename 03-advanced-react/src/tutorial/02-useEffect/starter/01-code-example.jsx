376a0c18fc2df95956a38e6c39b430a1768eea80
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2025-01-13 16:50:48 +0700

    Add more!

--- 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  // const sayHello = () => {
+  //   console.log("Hello, there!");
+  //   // Be careful, you will have infinite loop!
+  //   setValue(value + 1);
+  // };
+  // sayHello();
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
